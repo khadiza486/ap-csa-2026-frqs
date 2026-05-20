e53dc08c351c217c38ffd9bfd55ddd0d474f6292
khadiza486 <khadizaa2792@nycstudents.net>
2026-05-20 11:32:32 -0400

    Update account.java

--- question-1/account.java
+++ question-1/account.java
@@ -1,1 +1,10 @@
-
+public Account( String requestedName){
+    int num =1;
+    if(requestedName.isAvailable()){
+        username = requestedName;
+    } 
+    while(!requestedName.isAvailable()){
+        username = requestedName+num;
+        num++;
+    }
+}
