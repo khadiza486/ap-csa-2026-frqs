392109c7a1972a8adf253957a5bf145154740827
khadiza486 <khadizaa2792@nycstudents.net>
2026-06-15 22:17:32 -0400

    Update CourseRecord.java

--- question-3/CourseRecord.java
+++ question-3/CourseRecord.java
@@ -1,1 +1,24 @@
+public int moreHistoryThanMathAbsences()
+{
+    int count = 0;
 
+    for (int i = 0; i < historyList.size(); i++)
+    {
+        CourseRecord history = historyList.get(i);
+
+        for (int j = 0; j < mathList.size(); j++)
+        {
+            CourseRecord math = mathList.get(j);
+
+            if (history.getStudentID().equals(math.getStudentID()))
+            {
+                if (history.getAbsences() > math.getAbsences())
+                {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+}
